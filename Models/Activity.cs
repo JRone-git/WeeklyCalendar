57c4ed2cea3e709196debb0510514c34b6845500
Jonne Riepponen <117917255+JRone-git@users.noreply.github.com>
2024-12-04 12:50:10 +0200

    Update Activity.cs

--- Models/Activity.cs
+++ Models/Activity.cs
@@ -1,1 +1,11 @@
+namespace WeeklyCalendar.Models
+{
+    public class Activity
+    {
+        public int Id { get; set; }
+        public required string DayOfWeek { get; set; }
+        public required string Description { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
  
